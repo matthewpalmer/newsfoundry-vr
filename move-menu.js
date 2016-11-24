c3aaa454b8e6c5264867dfe8c31682f8e71549e8
Palmer, Matthew <Matthew.Palmer2@news.com.au>
2016-11-24 11:59:17 +1100

    add menu with shelf

--- move-menu.js
+++ move-menu.js
@@ -3,31 +3,68 @@ AFRAME.registerComponent('move-menu', {
     on: {type: 'string'}
   },
 
+  animateToPos: function(box, pos) {
+    var anim = document.createElement('a-animation');
+    anim.setAttribute('attribute', 'position');
+    // -5 1 -2.5
+    anim.setAttribute('to', pos);
+    anim.setAttribute('dur', '700');
+    anim.setAttribute('easing', 'ease-in-out-cubic');
+
+    var anim2 = document.createElement('a-animation');
+    anim2.setAttribute('attribute', 'scale');
+    anim2.setAttribute('to', '1 1 1');
+    anim2.setAttribute('dur', '700');
+    anim2.setAttribute('easing', 'ease-in-out-cubic');
+
+    var anim3 = document.createElement('a-animation');
+    anim3.setAttribute('attribute', 'rotation');
+    anim3.setAttribute('to', '0 0 0');
+    anim3.setAttribute('dur', '700');
+    anim3.setAttribute('easing', 'ease-in-out-cubic');
+
+    var old = box.getElementsByTagName('a-animation');
+    box.removeChild(old[0]);
+
+    box.appendChild(anim);
+    box.appendChild(anim2);
+    box.appendChild(anim3);
+  },
+
+  animateToCorner: function(box, i) {
+    console.log('fdhafjdkhsafsd');
+    var curpos = box.getAttribute('position');
+    var newpos = -4 + ' 1.45 ' + (-3 + i * 0.75);
+    this.animateToPos(box, newpos);
+  },
+
+  animateToMiddle: function(box) {
+    this.animateToPos(box, "0 2.5 -4.5");
+  },
+
+
   init: function () {
     var data = this.data;
     var el = this.el;
+    var that = this;
 
     el.addEventListener(data.on, function () {
-      var boxes = document.getElementsByTagName('a-box');
-      for (var i = 0; i < boxes.length; i++) {
-        var curpos = boxes[i].getAttribute('position');
-        var anim = document.createElement('a-animation');
-        anim.setAttribute('attribute', 'position');
-        anim.setAttribute('to', -2 + (i * 2) + ' -1 ' + -3);
-        anim.setAttribute('dur', '700');
-        anim.setAttribute('easing', 'ease-in-out-cubic');
+      var boxes = Array.prototype.slice.call(document.getElementsByTagName('a-box'), 0, 4);
+      boxes.splice(boxes.indexOf(el), 1);
+      console.log(boxes);
+      boxes.forEach(function(b, i) {
+        that.animateToCorner(b, i);  
+      });
 
-        var old = boxes[i].getElementsByTagName('a-animation');
-        old[0].setAttribute('dur', '25500');
-
-        boxes[i].appendChild(anim);
-      }
+      that.animateToMiddle(el);
 
       getStory('09b7545627b783c80b54525401142f16', function(json) {
         console.log('will speak', json.title);
         speakText(json.title);
       });
 
+      // melbourne story
+      // 7373b1109acf4e474fa01421adf27036
       // real estate
       //http://cdn.newsapi.com.au/content/v2/09b7545627b783c80b54525401142f16?api_key=gqz7h5zs6b9f5bxrwhpgfhhb
       //top news
